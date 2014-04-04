1d61178c036d8a99ff5af01062f7c20a628e9e72
apunicorn <anyapriestley@gmail.com>
2014-04-04 11:44:52 -0400

    name chooser working properly now

--- calendar.js
+++ calendar.js
@@ -6,16 +6,38 @@ $(function() {
         $(this).toggleClass('clicker');
     });
 
-    $('#submit_button').click(function(){
+  $('#submit_button').click(function(){
         var div1 = $('<div></div>');
         $('#ready_names').append(div1);
-        div1.html($('#name_input').val());
+        var chosenName = $('#name_input').val();
+        div1.html(chosenName);
         var chosenColor = $('#color_select').val();
         div1.css({
-            'font-weight' : 'bold',
-            'color' : chosenColor
+            'font-weight' : 'normal',
+            'color' : chosenColor,
+            'padding' : '6px'
         });
-
+        div1.attr('id', chosenName);
+        if($('#ready_names').children()){
+            var nameArray = $('#ready_names').children();
+            var newArray = [];
+            for(var ii=0; ii<nameArray.length; ii++){
+                (function(idx) {
+                    $(nameArray[idx]).off('click').click(function(){
+                        var newestArray = [];
+                        newArray.unshift($(nameArray[idx]));
+                        $(newArray[0]).css({'text-decoration' : 'underline', 'font-weight' : 'bold'});
+                        if(newArray.length > 1){
+                            newestArray = newArray.slice(1);
+                            for(var jj=0; jj<newestArray.length; jj++){
+                                $(newestArray[jj]).css({'text-decoration' : 'none', 'font-weight' : 'normal'});
+                                newestArray = [];
+                  }
+            } 
+                    });
+                })(ii);
+            }
+        }
     });
 
 
@@ -25,6 +47,5 @@ $(function() {
 
 
 
-
-
  });
+
